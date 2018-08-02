0bde4f39b55d2cd09bb85d7a9a17341d2acd53c1
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2018-08-02 17:23:44 -0400

    change column name

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured)
+INSERT INTO burgers (name, devoured)
 VALUES ("Avocado Burger", false);
-INSERT INTO burgers (burger_name, devoured)
+INSERT INTO burgers (name, devoured)
 VALUES ("Macaroni Burger", false);
-INSERT INTO burgers (burger_name, devoured)
+INSERT INTO burgers (name, devoured)
 VALUES ("Pizza Burger", false);
 
 SELECT * FROM burgers;
