51e1be7c622609c0f349ac36b49a37851b6c3958
Christina Gerard <ChristinaGerard@Christinas-MBP.attlocal.net>
2018-08-02 19:35:47 -0400

    port

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
+// var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 8889,
     user: "root",
     password: "root",
     database: "burgers_db",
   });
 }
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   port: 8889,
+//   password: "root",
+//   database: "burgers_db",
+// });
+
+
 
 
 // Make connection.
